61440a83e09c60b392fa63b8d88f7159ed76748b
Filip <filip.marusic@gmail.com>
2020-04-27 14:00:15 +0200

    drastically reduce RAM consumption by only storing image histograms in a list

--- homework2/colordetect.py
+++ homework2/colordetect.py
@@ -31,24 +31,33 @@ if __name__ == "__main__":
     path = os.path.dirname(os.path.realpath(__file__)) + '/pictures/'
     colors = ['red', 'green', 'blue', 'yellow', 'white', 'black']
     en_colors = enumerate(colors)
-    images = []
     images_color = []
+    hue_hists = []
+    saturation_hists = []
+    value_hists = []
     for c in en_colors:
         p = path + c[1] + '/*'
-        imgs = ([cv2.imread(file) for file in glob.glob(p)])
-        # images are in BGR format!
-        images.extend(imgs)
-        num_imgs = len(imgs)
+        num_imgs = 0
+        # imgs = ([cv2.imread(file) for file in glob.glob(p)])
+        for file in glob.glob(p):
+            img = cv2.imread(file)
+            # images are in BGR format!
+            # convert to HSV
+            imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+            # histogram calculation
+            # HSV ranges :: H: 0-179, S: 0-255, V: 0-255
+            hh = np.concatenate(calc_hist(imgHSV, 0, 180)) 
+            hue_hists.append(hh)
+            sh = np.concatenate(calc_hist(imgHSV, 1, 256))
+            saturation_hists.append(sh)
+            vh = np.concatenate(calc_hist(imgHSV, 2, 256))
+            value_hists.append(vh)
+            # TODO: check model perf. for alternative number of bins
+
+            num_imgs += 1
+
         images_color.extend(num_imgs * [c[0]]) # list of encoded color labels (e.g. 0 for 'red')
-
-    # convert to HSV
-    imagesHSV = [cv2.cvtColor(img, cv2.COLOR_BGR2HSV) for img in images]
-
-    # HSV ranges :: H: 0-179, S: 0-255, V: 0-255
-    # for testing: calc HSV hists for the 1. picture
-    # hue_hist = calc_hist(imagesHSV[0], 0, 180)
-    # saturation_hist = calc_hist(imagesHSV[0], 1, 256)
-    # value_hist = calc_hist(imagesHSV[0], 2, 256)
 
     # visualize histograms
     # plt.plot(hue_hist)
@@ -58,19 +67,6 @@ if __name__ == "__main__":
     # plt.title(colors[images_color[0]])
     # plt.show()
 
-    # histogram calculation for every image
-    hue_hists = []
-    saturation_hists = []
-    value_hists = []
-    for img in imagesHSV:
-        # TODO: check model perf. for alternative number of bins
-        hh = np.concatenate(calc_hist(img, 0, 180))
-        hue_hists.append(hh)
-        sh = np.concatenate(calc_hist(img, 1, 256))
-        saturation_hists.append(sh)
-        vh = np.concatenate(calc_hist(img, 2, 256))
-        value_hists.append(vh)
-    
     # training based on hue
     X, y = hue_hists, images_color
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
