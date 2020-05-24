940d50052fdd5d68d955305397936eebd7e59792
Filip <filip.marusic@gmail.com>
2020-05-24 16:00:55 +0200

    working SVC face recognition

--- homework3/face_recog.py
+++ homework3/face_recog.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import glob
 from sklearn.svm import SVC
 from sklearn.metrics import plot_confusion_matrix
+from sklearn import preprocessing
 from torchvision.transforms import ToTensor
 from PIL import Image
 
@@ -13,9 +14,7 @@ from PIL import Image
 from facenet_pytorch import InceptionResnetV1
 
 # generate claases
-face_classes = []
-for i in range(0,20):
-    face_classes.append('face_' + str(i))
+face_classes = ['face_' + str(i) for i in range(0,21)]
 
 # load pretrained face recognition model
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
@@ -37,7 +36,7 @@ for f_c in face_classes:
         # calculate embeddings
         img_embedding = resnet(img.unsqueeze(0))
 
-        X_train.append(img_embedding)
+        X_train.append(img_embedding.detach().numpy().ravel())
         y_train.append(f_c)
 
     test_path = path + 'test_data/' + f_c + '/*'
@@ -48,15 +47,26 @@ for f_c in face_classes:
         # calculate embeddings
         img_embedding = resnet(img.unsqueeze(0))
 
-        X_test.append(img_embedding)
+        X_test.append(img_embedding.detach().numpy().ravel())
         y_test.append(f_c)
 
-# setup SVC
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+# convert labels to integers
+le = preprocessing.LabelEncoder()
+le.fit(face_classes)
+y_train = le.transform(y_train)
+y_test = le.transform(y_test)
 
 # train SVC on embeddings
+model = SVC(kernel='linear')
+model.fit(X_train, y_train)
 
-# test SVC
-
-# plot confusion matrix
+# test SVC + plot confusion matrix
+plot_confusion_matrix(model, X_test, y_test)
+plt.show()
 
 # save model
