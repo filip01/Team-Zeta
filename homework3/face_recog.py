4dfac398188bc744f131c0b911b90b6a7dadfb47
Filip <filip.marusic@gmail.com>
2020-05-24 14:49:51 +0200

    wip: recognition

--- homework3/face_recog.py
+++ homework3/face_recog.py
@@ -3,6 +3,7 @@ import torch.nn
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
 from sklearn.svm import SVC
 from sklearn.metrics import plot_confusion_matrix
 from torchvision.transforms import ToTensor
@@ -11,17 +12,44 @@ from PIL import Image
 # import pretrained model:
 from facenet_pytorch import InceptionResnetV1
 
-'''
-image = Image.open(img_path)
-image = ToTensor()(image).unsqueeze(0) # unsqueeze to add artificial first dimension
-'''
-
-# load data
+# generate claases
+face_classes = []
+for i in range(0,20):
+    face_classes.append('face_' + str(i))
 
 # load pretrained face recognition model
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
 
-# calculate embeddings
+# load data
+path = os.path.dirname(os.path.realpath(__file__)) + '/faces/'
+
+X_train = []
+y_train = []
+X_test = []
+y_test = []
+
+for f_c in face_classes:
+    training_path = path + 'training_data/' + f_c + '/*'
+    for file in glob.glob(training_path):
+        img = Image.open(file)
+        img = img.resize((160, 160))
+        img = ToTensor()(img)
+        # calculate embeddings
+        img_embedding = resnet(img.unsqueeze(0))
+
+        X_train.append(img_embedding)
+        y_train.append(f_c)
+
+    test_path = path + 'test_data/' + f_c + '/*'
+    for file in glob.glob(test_path):
+        img = Image.open(file)
+        img = img.resize((160, 160))
+        img = ToTensor()(img)
+        # calculate embeddings
+        img_embedding = resnet(img.unsqueeze(0))
+
+        X_test.append(img_embedding)
+        y_test.append(f_c)
 
 # setup SVC
 
